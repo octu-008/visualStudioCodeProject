295af1189faa09ebe0af0c5ae5b2d6f2a49145cb
04171117刘镇滔 <lzm402931417@163.com>
2020-08-09 16:20:03 +0800

    submit:20200809

---
+++ cli_test/src/components/newComponent.vue
@@ -0,0 +1,24 @@
+<template>
+<div class="red-text">
+  <h2>props属性:{{msg}}</h2>
+  <h2>data属性:{{data_msg}}</h2>
+</div>
+</template>
+<script>
+ export default {
+   name:'NewComponent',
+   props:{
+     msg: String
+   },
+   data() {
+     return {
+       data_msg: this.msg
+     }
+   },
+}
+</script>
+<style scoped>
+.red-text{
+  color: red;
+}
+</style>
